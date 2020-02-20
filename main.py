9a7f0061eba258942f6a0942c7a72e9971634b7d
Ihsara <longchau21@gmail.com>
2020-02-20 12:24:40 +0200

    Update dependencies

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import requests
-endpoint = ".../api/ip"
-data = {"ip": "1.1.2.3"}
-headers = {"Authorization": "Bearer MYREALLYLONGTOKENIGOT"}
+import json
 
-print(requests.post(endpoint, data=data, headers=headers).json())
+with open('.env_var.json') as envfile:
+    env_var = json.load(envfile)
+
+with open('instance.json') as datafile:
+    data = json.load(datafile)
+endpoint = env_var['address']
+headers = {"Authorization": env_var['token']}
+
+print(data)
+
+print(requests.post(endpoint, json=data, headers=headers).json())
